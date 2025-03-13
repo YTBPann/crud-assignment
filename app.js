5c652d4729b0853ccbeaedcbe2c8092da609bfd4
Pann <himari.fb@gmail.com>
2025-03-13 11:23:57 +0700

    Replace with new code and reinitialize Git

---
+++ app.js
@@ -0,0 +1,46 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose')
+let {CreateErrorRes} = require('./utils/responseHandler')
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+mongoose.connect("mongodb://localhost:27017/S5");
+mongoose.connection.on('connected',()=>{
+  console.log("connected");
+})
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/products', require('./routes/products'));
+app.use('/categories', require('./routes/category'));
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+
+  CreateErrorRes(res,err.message,err.status||500);
+});
+
+module.exports = app;
